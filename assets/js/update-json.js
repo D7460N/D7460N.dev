d61ff96760958ad6376cc35ca66c0bfc1718f0e3
D7460N <80736+dragontheory@users.noreply.github.com>
2023-05-18 22:21:13 -0400

    Update update-json.js

--- assets/js/update-json.js
+++ assets/js/update-json.js
@@ -1,35 +1,25 @@
-// This script fetches tweets and writes them to a JSON file
-
-// import required packages
 const Twit = require('twit');
 const fs = require('fs');
 
-// set up Twit with your Twitter API keys
 const T = new Twit({
-  consumer_key: process.env.TWITTER_API_KEY,
-  consumer_secret: process.env.TWITTER_API_SECRET_KEY,
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+  timeout_ms: 60*1000,
+  strictSSL: true,
 });
 
-// specify parameters for the search
-let params = {
-  q: 'from:@dragontheory',  // replace @yourTwitterHandle with your actual handle
-  count: 10  // get 10 tweets
-}
-
-// perform the search and save results to a JSON file
-T.get('search/tweets', params, (err, data, response) => {
+T.get('statuses/user_timeline', { screen_name: 'your_twitter_handle', count: 10 }, (err, data, response) => {
   if (err) {
     console.error('Error fetching tweets:', err);
   } else {
-    const tweets = data.statuses.map(status => ({
-      text: status.text,
-      created_at: status.created_at,
-      url: `https://twitter.com/${status.user.screen_name}/status/${status.id_str}`
+    const articles = data.map(tweet => ({
+      // transform tweet data to your article schema
+      // ...
     }));
-    fs.writeFile('./assets/js/articles.json', JSON.stringify(tweets, null, 2), (err) => {
+
+    fs.writeFile('./assets/js/articles.json', JSON.stringify(articles, null, 2), (err) => {
       if (err) {
         console.error('Error writing to file:', err);
       } else {
