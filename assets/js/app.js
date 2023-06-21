5cb3ec0ecf6e34f585aa922a19eb2447e9458f75
D7460N <dragon@dragontheory.com>
2023-06-21 11:14:10 -0400

    Removed leading forward slash for URL, Added comment ref

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
+// PWA Tutorial for Beginners https://www.youtube.com/watch?v=egmwY9n6qWY
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('sw.js')
     .then((reg) => console.log('service worker registered', reg))
     .catch((err) => console.log('service worker not registered', err))
 }
--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+// PWA Tutorial for Beginners https://www.youtube.com/watch?v=egmwY9n6qWY
+
 // install service worker
 self.addEventListener('install', evt => {
   console.log('service worker has been installed')
