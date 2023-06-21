6caf99d43530410db116c0259156093ca711607b
D7460N <dragon@dragontheory.com>
2023-06-21 00:10:30 -0400

    Added PWA maskable icons, added meta theme-color

---
+++ sw.js
@@ -0,0 +1,9 @@
+// install service worker
+self.addEventListener('install', evt => {
+  console.log('service worker has been installed')
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  console.log('service worker has been activated')
+});
